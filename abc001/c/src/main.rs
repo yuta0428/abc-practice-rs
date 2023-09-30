8c3c7c28c4da7c1843741862bca628736d16134a
yuta0428 <yuta_okamoto428@yahoo.co.jp>
2023-10-01 00:38:02 +0900

    abc001/c answer

--- abc001/c/src/main.rs
+++ abc001/c/src/main.rs
@@ -1,11 +1,34 @@
 use proconio::input;
+/* Answer https://atcoder.jp/contests/abc001/submissions/26387527
+fn main() {
+    input!{
+        deg:usize,
+        dis:usize,
+    }
+    
+    let lg = vec!["NNE","NE","ENE","E","ESE","SE","SSE","S","SSW","SW","WSW","W","WNW","NW","NNW","N"];
+    let ls = vec![3,16,34,55,80,108,139,172,208,245,285,327];
 
+    let mut w = 0;
+    let n:i32 = (dis as f64 / 60.0 * 10.0).round() as i32;
+    for i in ls {
+        if n >= i { w += 1; }
+        else { break; }
+    }
+
+    let mut dir = "N";
+    if w == 0 { dir = "C"; }
+    else if deg * 10 > 1125 { dir = lg[(deg * 10 - 1125) / 2250]; }
+
+    println!("{} {}", dir, w);
+}
+ */
 fn main() {
     input! {
         deg: f32,
         dis: f32
     }
-    
+
     let disp = (dis / 60.0 * 10.0).round() / 10.0;
     let mut w = 0;
     if disp >= 0.3 { w += 1;}
