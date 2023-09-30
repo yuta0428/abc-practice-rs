33b7e8cb62d8c831932217349fa4ede4356b2f2a
yuta0428 <yuta_okamoto428@yahoo.co.jp>
2023-09-30 18:51:58 +0900

    Add abc001/a

---
+++ abc001/a/ojsubmit.sh
@@ -0,0 +1,3 @@
+
+oj https://atcoder.jp/contests/abc001/tasks/abc001_1 s src/main.rs
+    
---
+++ abc001/a/src/main.rs
@@ -0,0 +1,10 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        h1: i32,
+        h2: i32,
+    }
+    
+    println!("{}", h1 - h2);
+}
